b57e3a727fbd72c9aa95631f938883da7c2a0b77
anch <anch85072@gmail.com>
2026-04-10 10:33:03 +0300

    Доделал все сущности

--- src/main/java/com/example/jablog/entity/Board.java
+++ src/main/java/com/example/jablog/entity/Board.java
@@ -1,6 +1,9 @@
 package com.example.jablog.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.NaturalId;
@@ -21,8 +24,20 @@ public class Board {
     @Column(name="name", nullable = false, unique = true, length = 3)
     private String name;
 
-    @Column(name="rules", nullable = false) //chek
-    private String[] rules;
+    @Column(name="rules", nullable = false,
+            check = @CheckConstraint(constraint = "rules <@ ARRAY['r', 'w', '-']::char(1)[]"))
+    @Size(min = 6, max = 6)
+    private String[] rules = {"r","w","r","w","r","w"};
+
+    @Column(name="lifeCycleThreads",  nullable = false)
+    @Max(28)
+    @Min(0)
+    private int lifeCycleThreads = 14;
+
+    @Column(name="lifeCyclePosts", nullable = false,
+            check = @CheckConstraint(constraint = "life_cycle_posts<life_cycle_threads"))
+    @Min(1)
+    private int lifeCyclePosts = 7;
 
     @OneToMany(mappedBy = "board", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
     private Set<Threads> threads = new HashSet<Threads>();
