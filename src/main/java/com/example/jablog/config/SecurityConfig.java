06b5d0c565cc72bbc6cee6c2f9645d9d2621cb27
anch <anch85072@gmail.com>
2026-04-13 00:47:40 +0300

    сделал хуйню

--- src/main/java/com/example/jablog/config/SecurityConfig.java
+++ src/main/java/com/example/jablog/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -14,10 +15,14 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
+                .csrf(AbstractHttpConfigurer::disable)
+
+                .sessionManagement(session ->
+                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest().permitAll()
-                )
-                .formLogin(AbstractHttpConfigurer::disable);
+                );
 
         return http.build();
     }
