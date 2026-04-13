86de982716074d1fb36749af9ec46237b9853bbe
anch <anch85072@gmail.com>
2026-04-14 01:28:15 +0300

    Я рот ебал, почему оно не добавилось-то автоматом.

---
+++ src/main/java/com/example/jablog/config/security/CustomUserDetails.java
@@ -0,0 +1,73 @@
+package com.example.jablog.config.security;
+
+import com.example.jablog.entity.Board;
+import com.example.jablog.entity.Users;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+public class CustomUserDetails implements UserDetails {
+
+    private String boardName;
+    private String[] boardRules;
+    private String password;
+    private String nickname;
+    private String role;
+    private long id;
+
+    public static CustomUserDetails build(Users users){
+        Board board = users.getBoard();
+
+        return new CustomUserDetails(
+            board.getName(),
+            board.getRules(),
+            users.getPassword(),
+            users.getNickname(),
+            users.isRole() ? "ROLE_ADMIN" : "ROLE_GROUP",
+            users.getId()
+        );
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of (new SimpleGrantedAuthority(this.role));
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.nickname;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+}
