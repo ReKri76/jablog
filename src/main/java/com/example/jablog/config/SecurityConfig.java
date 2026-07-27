d2b027af4a779b27fba5424c53fc8e3e557712e2
anch <anch85072@gmail.com>
2026-07-27 23:57:25 +0300

    Сделал систему кармы.

--- src/main/java/com/example/jablog/config/SecurityConfig.java
+++ src/main/java/com/example/jablog/config/SecurityConfig.java
@@ -121,9 +121,9 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.GET, "/{boardName}")
                             .access(getterAuthorizationManager)
 
-                        .requestMatchers(HttpMethod.PATCH, "/api/carma/plus/{boardName}/{thread")
+                        .requestMatchers(HttpMethod.PATCH, "/api/carma/plus/{boardName}/{thread}")
                             .access(getterAuthorizationManager)
-                        .requestMatchers(HttpMethod.PATCH, "/api/carma/plus/{boardName}/{thread")
+                        .requestMatchers(HttpMethod.PATCH, "/api/carma/plus/{boardName}/{thread}")
                             .access(getterAuthorizationManager)
 
                         .anyRequest().denyAll()
