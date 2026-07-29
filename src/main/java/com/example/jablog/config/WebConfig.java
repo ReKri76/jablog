b47ce2b9f1bdb4e08da2004ce2f218bbc1e00f73
anch <anch85072@gmail.com>
2026-07-29 11:57:33 +0300

    Тест для айпи рейт лимита.

--- src/main/java/com/example/jablog/config/WebConfig.java
+++ src/main/java/com/example/jablog/config/WebConfig.java
@@ -17,6 +17,6 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(rateLimitInterceptor)
-                .addPathPatterns("/api/carma/*");
+                .addPathPatterns("/api/carma/**");
     }
 }
